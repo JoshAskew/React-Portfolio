471ae97d4296a7310c8b9bb278c621250f6ceae8
JoshAskew <asksprit3@gmail.com>
2024-10-23 23:41:41 -0500

    done for the night, forgot to commit all night

--- portfolio/src/components/Resume.jsx
+++ portfolio/src/components/Resume.jsx
@@ -3,16 +3,32 @@ import React from 'react';
 function Resume() {
   return (
     <section>
-      <h2>Resume</h2>
-      <a href="/path-to-resume.pdf" download>Download Resume</a>
+      <h2 className='resume-header'>Resume</h2>
+      <a href="https://docs.google.com/document/d/14s2cUop3L5g-py8BYRwTCmaHQApOyHOXYMxclbPxWZ0/edit?usp=sharing" download>Download Resume</a>
       <h3>Proficiencies</h3>
+      <h5>Front End</h5>
       <ul>
-        <li>React</li>
-        <li>JavaScript</li>
-        <li>Node.js</li>
         <li>CSS</li>
-        {/* Add more skills */}
+        <li>HTML</li>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>TypeScript</li>
+        <li>Bootstrap</li>
+        <li>RESTful APIs</li>
       </ul>
+      <h5>Back End</h5>
+      <ul>
+        <li>JavaScript</li>
+        <li>postgresSQL</li>
+        <li>SQL Databases</li>
+        <li>Express.js</li>
+        <li>RESTful APIs</li>
+        <li>Insomnia</li>
+      </ul>
+      <h5>IDEs</h5>
+      <li>Visual Studio Code</li>
+      <h5>Version Control</h5>
+      <li>Git</li>
     </section>
   );
 }

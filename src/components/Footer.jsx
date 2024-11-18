8fee72393b9c7b98ed688b8ad25fa65d55617693
JoshAskew <asksprit3@gmail.com>
2024-11-17 18:02:42 -0600

    changed link to facebook

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import github from '../images/github.png';
 import linkedin from '../images/linkedin.png';
-import stackoverflow from '../images/stackoverflow.png';
+import facebook from '../images/facebook.png';
 
 function Footer() {
   return (
@@ -9,8 +9,8 @@ function Footer() {
       <div className='footer-child'>
       <p className= 'follow'>Follow me on:</p>
       <a href="https://github.com/JoshAskew" target="_blank"> <img src={github} alt="GitHub icon"  className="social-icon"/></a>
-      <a href="https://linkedin.com" target="_blank"><img src={linkedin} alt="LinkedIn icon" className="social-icon"/></a>
-      <a href="https://stackoverflow.com" target="_blank"><img src={stackoverflow} alt="StackOverflow icon" className="social-icon"/></a>
+      <a href="https://linkedin.com/in/joshua-askew-0293bb338" target="_blank"><img src={linkedin} alt="LinkedIn icon" className="social-icon"/></a>
+      <a href="https://www.facebook.com/josh.askew.71" target="_blank"><img src={facebook} alt="Facebook icon" className="social-icon facebook"/></a>
       </div>
       <p className='copyright'>© {new Date().getFullYear()} Josh Askew. All rights reserved.</p>
     </footer>
